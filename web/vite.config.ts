ef76210772f911c631d29f38b89585f048a834b9
yumi <z1181806270@gmail.com>
2026-01-15 22:58:31 +0800

    feat(web): 端口根据env读取

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173,
-  },
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, '.', '')
+  
+  return {
+    plugins: [vue()],
+    server: {
+      host: env.VITE_DEV_HOST || '127.0.0.1',
+      port: parseInt(env.VITE_DEV_PORT || '5173'),
+    },
+  }
 })
